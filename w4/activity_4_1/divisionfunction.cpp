f54fe66cbbfa69d5f03ea784cd6991bbee2030db
Jeanne005 <ajdrake5096@gmail.com>
2025-11-07 01:50:34 +0000

    stack

---
+++ w4/activity_4_1/divisionfunction.cpp
@@ -0,0 +1,11 @@
+
+
+
+    int divide_int (int a, int b){
+
+    return a/b ;
+    }
+int main() { 
+    divide_int(4,2);
+    return 0;
+}
--- w4/activity_4_1/sets.cpp
+++ w4/activity_4_1/sets.cpp
@@ -2,17 +2,27 @@
 #include <array> // includes the array library
 #include <cstdio> // includes the cstudio library
 #include <string> // includes the string library
+#include <vector> // includes the vector library
+
 
 int main() {
-   std::array <int, 3> a = {1,2,3,};
+   std::array <int, 3> a = {1,2,3};
    std::printf("1,2,3");
 
-   std::array <char,2>dog = {'h','i',};
+   std::array <char,2>dog = {'h','i'};
    std::printf("%c\n", dog.at(1));
    
    std::array <bool,3> b ={true,false,true};
    bool first_element = b.at(0);
    std::printf("%d\n", first_element);
+
+   std::vector<int> squid = {1,2,3};
+   std::printf (%d,squid.at(0));
+
+   std::vector<char> dog = {'h','i'}
+
+
+
 
    
    return 0;
